22392f02bba80ddaaa1e6574526536038ab2e01a
vsisquesc <vsisques@gmail.com>
2024-03-15 23:10:35 +0100

    Updates

--- _nuxt/default.DFcYtGD4.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,a,L as o,o as c}from"./entry.BByD4D75.js";const d={},r={class:"wrapper"},n={class:"body"};function _(e,l){return c(),t("div",r,[a("div",n,[o(e.$slots,"default",{},void 0,!0)])])}const i=s(d,[["render",_],["__scopeId","data-v-9d0e0816"]]);export{i as default};
