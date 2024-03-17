79810b036074639118b44ff8154ef0d1d2937b45
vsisquesc <vsisques@gmail.com>
2024-03-17 19:32:18 +0100

    tarri

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   
+  <div class="bg"/>
   <div class="wrapper">
  
-    <div class="body">
       <slot />
-    </div>
  
   </div>
 </template>
@@ -14,15 +13,20 @@
 
 
 <style lang="postcss" scoped>
-.wrapper{
- 
+.bg{ 
+  @apply w-screen h-screen fixed;
   background-image: url('~/assets/img/periodico_clasico.jpg'), url('~/assets/img/poco_envejecido.jpg'), url('~/assets/img/prensa_fria_limpia.jpg');
   background-blend-mode: multiply, multiply;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
-  background-size: cover;
-  /* background-color: #000000; */
+  background-size: cover; 
+  /* background-attachment: fixed;  */
+  /* translate: 0 10% */
+}
+.wrapper{
+  @apply w-full h-full;
+  
 }
 .header{}
 .body{}
@@ -30,10 +34,20 @@
 </style>
 
 <style lang="postcss" >
+html, body, #__nuxt
+{
+    height: 100%;
+    width: 100%;
+}
+
 h1 {
   @apply text-3xl md:text-4xl  font-bold;
 }
-.text {
-  @apply font-TT2020 mix-blend-multiply text-zinc-700;
+h1,
+h2,
+h3,
+p ,
+span {
+  @apply font-TT2020 mix-blend-multiply text-black/65;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,96 @@
 <template>
-    <div class="content">
-      <h1 class="title text">Víctor Sisqués Cortés</h1>
-      <div class="links text">
-        <nuxt-link to="/wip">Projects</nuxt-link>
-        <nuxt-link to="/wip">About me</nuxt-link>
-        <nuxt-link to="/wip">Tarari</nuxt-link>
+  <div class="content">
+    <transition name="slide-T2B">
+      <div v-show="option==='projects'" class="projects">
+        <!-- https://image.shutterstock.com/shutterstock/photos/487752658/display_1500/stock-photo-vintage-looking-office-folder-with-numbered-tabs-isolated-on-white-487752658.jpg -->
+        <div class="tab"><p>CSS</p></div>
+        <div class="tab"><p>VISION</p></div>
+        <div class="tab"><p>APPS</p></div>
+        <div class="tab"><p>MISC</p></div>
       </div>
-      
+    </transition>
+
+    <div class="menu">
+      <h1 class="title">Víctor Sisqués Cortés</h1>
+      <div class="options">
+        <p @click="option = 'projects'">Projects</p>
+        <p @click="option = 'about_me'">About me</p>
+        <p @click="option = 'blog'">Blog</p>
+        <p @click="option = 'contact'">Contact</p>
+      </div>
     </div>
+ 
+
+    <div class="contact">
+      <p>LINKED IN</p>
+      <p>INSTAGRAM</p>
+      <p>EMAIL</p>
+      <p>TELEPHONE</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: "default",
 });
-</script>C
+
+
+const option: globalThis.Ref<string | undefined> = ref(undefined);
+</script>
 
 <style lang="postcss" scoped>
-
 .content{
-  @apply flex items-center justify-center w-full h-screen flex-col ;
+  @apply h-full w-full overflow-hidden;
+}
+
+.menu{
+  @apply w-full h-full flex flex-col gap-4 justify-center items-center;
+}
+
+.options{
+  @apply flex flex-col justify-center items-center;
+}
+.options > *{
+  @apply hover:italic text-2xl font-bold;
+}
+
+.projects{
+  @apply flex flex-row  text-2xl font-bold;
+  background-color:#000000;
+  mix-blend-mode: multiply;
+}
+
+.tab {
+  @apply w-[15%] px-12 py-2;
+  background-image: url('~/assets/img/tab.jpg');
+  background-size: contain;
+  position: relative;
+}
+
+.tab:not(:first-child)::after {
+  bottom: -99%;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  content: "";
+  display: block;
+  box-shadow:0px -10px 5px -2px rgba(0, 0, 0, 0.4)
+  
+  /* Ir aumentando el -10px por cada elemento de tab */
 }
  
-.title{
-  @apply my-4;
+/* .tab:nth-child(2)::after  {
+  box-shadow:0px -10px 5px -2px rgba(0, 0, 0, 0.4)
 }
+.tab:nth-child(3)::after  {
+  box-shadow:0px -12px 5px -2px rgba(0, 0, 0, 0.4)
+}
+.tab:nth-child(4)::after  {
+  box-shadow:0px -14px 5px -2px rgba(0, 0, 0, 0.4)
+} */
 
-.title,
-.links{
-  @apply flex w-full items-start md:items-center justify-center flex-col ;
-}
-a{
-  @apply hover:italic text-xl md:text-2xl font-bold;
-}
+ 
+
 </style>
