8b09da7710448ad6d8698a90450a611ab6f600b2
vsisquesc <vsisquesc@gmail.com>
2025-01-19 23:12:49 +0100

    wip

--- components/photos.vue
+++ components/photos.vue
@@ -48,60 +48,75 @@ onMounted(() => {
 })
 // ---------- Methods ----------
 function handleScroll() {
-    const rect_container = ImagesContainer.value!.getBoundingClientRect()
+    try {
+        const rect_container = ImagesContainer.value!.getBoundingClientRect()
 
-    // Distancia entre el inicio del div y el inicio del viewport
-    const d_top = -rect_container.top
-    const img_h = ImageScroller.value!.getBoundingClientRect().height
-    const h = rect_container.height
+        // Distancia entre el inicio del div y el inicio del viewport
+        const d_top = -rect_container.top
+        const img_h = ImageScroller.value!.getBoundingClientRect().height
+        const h = rect_container.height
 
-    // Porcentaje del div recorrido ( se tiene en cuenta la altura de la imagen)
-    const percent = d_top / (h - img_h)
+        // Porcentaje del div recorrido ( se tiene en cuenta la altura de la imagen)
+        const percent = d_top / (h - img_h)
 
-    // Limites para cada imagen
-    const step = 1 / images.length
+        // Limites para cada imagen
+        const step = 1 / images.length
 
-    // Se calcula q step mostrar
-    currentImageIndex.value = clamp(
-        Math.floor(percent / step),
-        0,
-        images.length
-    )
+        // Se calcula q step mostrar
+        currentImageIndex.value = clamp(
+            Math.floor(percent / step),
+            0,
+            images.length
+        )
+    } catch (err) {
+        console.log("Error while handling scroll", err)
+    }
 }
 
 function initImage(e: string): ImageScroller {
-    const containerWidth = ImagesContainer.value!.getBoundingClientRect().width
+    try {
+        const containerWidth =
+            ImagesContainer.value!.getBoundingClientRect().width
 
-    const img_h = ImageScroller.value!.getBoundingClientRect().height
-    const img_w = ImageScroller.value!.getBoundingClientRect().width
+        const img_h = ImageScroller.value!.getBoundingClientRect().height
+        const img_w = ImageScroller.value!.getBoundingClientRect().width
 
-    // Generacion de width
-    const min_w = img_w / 4
-    const max_w = img_w / 2
+        // Generacion de width
+        const min_w = img_w / 4
+        const max_w = img_w / 2
 
-    const width = between(min_w, max_w)
+        const width = between(min_w, max_w)
 
-    // Generacion de left
-    const min_left = 0
-    const max_left = containerWidth - width
+        // Generacion de left
+        const min_left = 0
+        const max_left = containerWidth - width
 
-    const left = between(min_left, max_left)
+        const left = between(min_left, max_left)
 
-    // Generacion de top
+        // Generacion de top
 
-    const height = (width * img_h) / img_w
+        const height = (width * img_h) / img_w
 
-    const min_top = 0
-    const max_top = img_h - height
+        const min_top = 0
+        const max_top = img_h - height
 
-    const top = between(min_top, max_top)
+        const top = between(min_top, max_top)
 
-    return {
-        src: e,
-        width: width,
-        top: top,
-        left: left,
-    } as ImageScroller
+        return {
+            src: e,
+            width: width,
+            top: top,
+            left: left,
+        } as ImageScroller
+    } catch (err) {
+        console.log("error while initializating images", err)
+        return {
+            src: e,
+            width: 0,
+            top: 0,
+            left: 0,
+        } as ImageScroller
+    }
 }
 
 function getStyle(img: ImageScroller, idx: number) {
@@ -119,6 +134,7 @@ function getStyle(img: ImageScroller, idx: number) {
 #container {
     height: 100vh;
     width: 100vw;
+    overflow-x: unset;
 }
 .image-scroller {
     height: 100vh;
